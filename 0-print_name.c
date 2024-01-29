a00b247adf5c675ecf1f70869a6e7cfd539006b0
Maggieboa <achymag2020@gmail.com>
2024-01-28 23:56:55 -0800

    0-print_name.c

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
 /**
@@ -10,7 +9,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 
-if (name != NULL && f != NULL)
+if (f != NULL)
 {
 	f(name);
 }
